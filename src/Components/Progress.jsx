12a1a95e09f8e19086209cde41112288256e92de
JayPatel202313 <jpatel@pursuit.org>
2024-04-14 15:26:13 -0400

    Complete

--- src/Components/Progress.jsx
+++ src/Components/Progress.jsx
@@ -1,3 +1,14 @@
-export default function Progress() {
-  return null;
+import React from 'react'
+
+export default function Progress(props) {
+  return (
+    <section className="progress">
+      <h2>
+        Raised <span className="secondary">${props.donations.reduce((t,a)=>{
+            return t + a.amount;
+        },0)}</span> of
+        <span className="secondary">$1000</span>
+      </h2>
+    </section>
+  )
 }
